2fd1e23458d1580f3c069cfe7f8c3e4901221fd1
Machyne <mcotter222@gmail.com>
2014-05-05 17:39:04 -0500

    hide info on got it

--- client/interaction.js
+++ client/interaction.js
@@ -3,6 +3,9 @@ if (Meteor.isClient) {
   Template.info.events({
     'keydown input#myNumber': function() {
       Session.set('clientDrawNumber', $('#myNumber').val());
+    },
+    'click button': function () {
+      $('#info').hide();
     }
   });
 

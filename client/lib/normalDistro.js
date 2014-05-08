8060655bf08fa46312291c0410feaf4b1aacb04a
David Pickart <pickartd@carleton.edu>
2014-05-08 03:03:43 -0500

    Debug probability
    Matt’s work

--- client/lib/normalDistro.js
+++ client/lib/normalDistro.js
@@ -3,8 +3,12 @@
  * adapted from http://www.math.ucla.edu/~tom/distributions/normal.html
  */
 function normalcdf(X){  //HASTINGS.  MAX ERROR = .000001
+  console.log(X);
   var T=1/(1+.2316419*Math.abs(X));
   var D=.3989423*Math.exp(-X*X/2);
+  console.log('d/t');
+  console.log(D);
+  console.log(T);  
   var Prob=D*T*(.3193815+T*(-.3565638+T*(1.781478+T*(-1.821256+T*1.330274))));
   if (X>0) {
     Prob=1-Prob;
@@ -23,8 +27,11 @@ normalProb = function (num, mean, stdev) {
         Prob = 1;
       }
     }else{
+      console.log(num-mean);
       Prob = normalcdf((num-mean)/stdev);
-      // Prob = round(100000*Prob)/100000;
+      console.log('P');
+      console.log(Prob);
+      Prob = round(100000*Prob)/100000;
     }
   }
   return Prob;
